5fc4ff2c2e36e395c95ac44dc73d585048c4aad0
havanaKvn <kevinrakotohavana@gmail.com>
2024-11-03 00:47:00 +0100

    PreProd Version: Extract Raw data from multiple sources

--- Data_Extract_Stage.py
+++ Data_Extract_Stage.py
@@ -1,7 +1,9 @@
 from datetime import date
+import pandas as pd
+import glob
 
 today = date.today()
-
+''' ### Delivered
 def extract_raw_transaction(file_path):
 
     stored_in = "C:/Users/kevin/OneDrive/Bureau/Personal_Project/Budget_Monitoring/Data/2_Silver/"
@@ -42,14 +44,58 @@ def extract_account_details(file_path):
             # Close the file
             file.close() 
         i +=1
+'''       
+###Development in progress
+def extract_raw_transaction(file_path,leng):
+
+    stored_in = "C:/Users/kevin/OneDrive/Bureau/Personal_Project/Budget_Monitoring/Data/1_Bronze/"
+    # Open and read the text file
+    with open(file_path, 'r') as file:
+        text = file.read()
+
+    # Split the text by two or more consecutive newline characters
+    # Each empty line is typically represented by '\n\n' in text files
+    chunks = text.split('\n\n') 
+    
+    for chunk in chunks:
+        # Open a file in write mode
+        file = open(stored_in + "Transactions_Extracted_" +str(leng)+"_"+ str(today)+ ".csv", "w")
+        # Write some text to the file
+        file.write(chunk)
+        # Close the file
+        file.close() 
+       
+
+def Combine_All_Files(path):
+
+    # Read all CSV files in the folder
+    all_files = glob.glob(path)
+
+    # List to store DataFrames
+    df_list = []
+    leng = len(all_files)
+    # Iterate over the list of files and read each one
+    for file in all_files:
+        leng = leng - 1
+        extract_raw_transaction(file,leng)
+       
         
-        
+        #df = pd.read_csv(file)
+        #df_list.append(df)
+
+    # Concatenate all DataFrames into one
+    #combined_df = pd.concat(df_list, ignore_index=True)
+
+        #print(leng)    
 
 if __name__ == "__main__":
     # Provide the path to your text file
-    file_path = 'C:/Users/kevin/OneDrive/Bureau/Personal_Project/Budget_Monitoring/Data/1_Bronze/0455501C0181728157083044.csv'
+    #file_path = 'C:/Users/kevin/OneDrive/Bureau/Personal_Project/Budget_Monitoring/Data/Source/1_Bronze/0455501C0181728157083044.csv'
+    #extract_raw_transaction(file_path)
+    #extract_account_details(file_path)
 
-    extract_raw_transaction(file_path)
-    extract_account_details(file_path)
-    
+    # Path to the folder containing CSV files
+    path = 'C:/Users/kevin/OneDrive/Bureau/Personal_Project/Budget_Monitoring/Data/1_Bronze/*.txt'
+    Combine_All_Files(path)
+
     print("Extract Succed on " + str(today))
