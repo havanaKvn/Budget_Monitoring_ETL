677d084781470a884fa13a8bcdc307a8dd3094be
havanaKvn <kevinrakotohavana@gmail.com>
2024-11-02 01:32:12 +0100

    PreProd Version: Fact Table Deliverde

--- Master_Data_Extract.py
+++ Master_Data_Extract.py
@@ -2,26 +2,10 @@ from datetime import date
 import pandas as pd
 import csv
 import uuid
+import datetime
 
 today = date.today()
 
-
-'''
-# Drop rows with missing values
-df.dropna(inplace=True)
-
-# Drop unnecessary columns (replace 'column_name' with actual column names)
-df.drop(columns=['column_name1', 'column_name2'], inplace=True)
-
-# Rename columns (replace 'old_name' and 'new_name' with actual names)
-df.rename(columns={'old_name1': 'new_name1', 'old_name2': 'new_name2'}, inplace=True)
-
-# Save the cleaned DataFrame back to a CSV file
-df.to_csv('cleaned_file.csv', index=False)
-
-print("Data cleaning complete. Cleaned file saved as 'cleaned_file.csv'.")
-
-'''
 # Provide the path to your text file   
 #file_path = "C:/Users/kevin/OneDrive/Bureau/Personal_Project/Budget_Monitoring/Data/2_Silver/Transactions_Extracted_" + str(today)+".txt"
 file_path = 'C:/Users/kevin/OneDrive/Bureau/Personal_Project/Budget_Monitoring/Data/2_Silver/Transactions_Extracted_2024-10-09.csv'
@@ -29,25 +13,15 @@ file_path = 'C:/Users/kevin/OneDrive/Bureau/Personal_Project/Budget_Monitoring/D
 #with open('Transactions_Extracted_2024-10-09.csv', 'r') as file:
     #text = file.read()
 
+# To convert date format
+def Extract_Long_Date(date_list):
+    date_object = datetime.datetime.strptime(date_list, '%d/%m/%Y')
+    #return date_object.strftime('%d/%B/%Y')
+    return date_object.strftime('%m/%d/%Y')
 
-def Fact_Transaction_Tab_Cleanning():
-    print("test")
-        # Load the CSV file into a DataFrame
-    df = pd.read_csv(file_path,delimiter=';',encoding='cp1252')
-    #print(df.head())
-
-    # Drop unnecessary columns (replace 'column_name' with actual column names)
-    #Bill_Raw = df.drop(columns=['Date', 'Montant(EUROS)'], inplace=True)
-
-    # Rename columns (replace 'old_name' and 'new_name' with actual names)
-    df.rename(columns={'Libellï¿½': 'Libele_Achat', 'Montant(EUROS)': 'Montant'}, inplace=True)
-
-    # Save the cleaned DataFrame back to a CSV file
-    #df.to_csv('cleaned_file.csv', index=False)
-
-    print(df)
-
-    print("Data cleaning complete. Cleaned file saved as 'cleaned_file.csv'.")
+def Convert_Price_Format(price):
+    new_value = price.replace(',', '.')
+    return new_value
 
 def Bill_Category(x):
     if  'ACHAT CB' in x:
@@ -64,8 +38,66 @@ def Bill_Category_Code(x):
         return 261
     else:
         return 111
+    
+def Fact_Transaction_Explicite():   
+    # Load the CSV file into a DataFrame
+    df = pd.read_csv(file_path,delimiter=';',encoding='cp1252')
+    #print(df.head())
+
+    # Rename columns (replace 'old_name' and 'new_name' with actual names)
+    df.rename(columns={'Libellï¿½': 'Bill_Label', 'Montant(EUROS)': 'Price'}, inplace=True)
+    
+    #Create Unique INT Id 
+    df['Id_Bill'] = range(1, len(df) + 1)
+    
+    #Define Bill Category
+    df['Bill_Category'] = df['Bill_Label'].apply(Bill_Category)
+    
+    #Define Bill Category
+    df['Id_Bill_Category'] = df['Bill_Category'].apply(Bill_Category_Code) 
+
+    #Define Long Date
+    df['Date_Formated'] = [Extract_Long_Date(date_) for date_ in df['Date']]
+
+    #Format Price
+    df['Price'] = [Convert_Price_Format(Price) for Price in df['Price']]
+                           
+    #test = df['Libele_Achat'].unique
+    
+    #Rearange Columns 
+    new_order = ['Date','Date_Formated', 'Bill_Label','Price','Id_Bill','Id_Bill_Category','Bill_Category']  
+    df = df[new_order] 
+
+    #print(df)
+
+    # Save the cleaned DataFrame back to a CSV file
+    df.to_csv('C:/Users/kevin/OneDrive/Bureau/Personal_Project/Budget_Monitoring/Data/2_Silver/Fact_Transaction_Explicite.csv', sep=';', index=False)
+
+    print("Data cleaning complete. Cleaned file saved as 'cleaned_file.csv'.")
+
+def Fact_Transaction_Tab_Cleanning():
+    
+    file_path = 'C:/Users/kevin/OneDrive/Bureau/Personal_Project/Budget_Monitoring/Data/2_Silver/Fact_Transaction_Explicite.csv'
+    # Load the CSV file into a DataFrame
+    df = pd.read_csv(file_path,delimiter=';',encoding='cp1252')
+ 
+    # Drop unnecessary columns (replace 'column_name' with actual column names)
+    df.drop(columns=['Date','Bill_Label', 'Bill_Category'], inplace=True)
+
+    # Rename columns (replace 'old_name' and 'new_name' with actual names)
+    df.rename(columns={'Date_Formated': 'Date'}, inplace=True)
+
+    #Rearange Columns 
+    new_order = ['Date','Id_Bill','Id_Bill_Category','Price']  
+    df = df[new_order]
+
+    # Save the cleaned DataFrame back to a CSV file
+    df.to_csv('C:/Users/kevin/OneDrive/Bureau/Personal_Project/Budget_Monitoring/Data/3_Gold/Fact_Transaction.csv', sep=';', index=False)
+
+    print(df)
 
 def Dim_Bill_Tab_Cleanning():
+
     # Load the CSV file into a DataFrame
     df = pd.read_csv(file_path, delimiter=';', encoding='cp1252')
     #print(df.head())
@@ -101,7 +133,9 @@ def Dim_Bill_Tab_Cleanning():
 
 if __name__ == "__main__":
     
-    Dim_Bill_Tab_Cleanning()
+    Fact_Transaction_Explicite()
+    #Dim_Bill_Tab_Cleanning()
+    Fact_Transaction_Tab_Cleanning()
     
     print("Extract Succed on " + str(today))
 
